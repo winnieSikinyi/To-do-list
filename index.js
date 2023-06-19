6e8ff95076b1ea263afbcf5e12577c4e0d6cce41
winnieSikinyi <winniesikinyi@gmail.com>
2023-06-19 17:17:24 +0300

    changed

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ document.getElementById('form').addEventListener('submit', async function(event)
     event.preventDefault();
     let firstTask = document.getElementById('first-task').value;
     let secondTask = document.getElementById('second-task').value;
-    // let thirdTask = document.getElementById('third-task').value;
+    let thirdTask = document.getElementById('third-task').value;
     let fourthTask = document.getElementById('fourth-task').value;
     let fifthTask = document.getElementById('fifth-task').value;
     let sixthTask = document.getElementById('sixth-task').value;
@@ -12,7 +12,7 @@ document.getElementById('form').addEventListener('submit', async function(event)
     let data = {
         firstTask:firstTask,
         secondTask: secondTask,
-        // thirdTask:thirdTask,
+        thirdTask:thirdTask,
         fourthTask:fourthTask,
         fifthTask:fifthTask,
         sixthTask:sixthTask,
@@ -45,3 +45,38 @@ fetch('https://dummyjson.com/todos/1', {
 })
 .then(res => res.json())
 .then(console.log);
+const completeBtn = taskItem.querySelector('.complete-btn');
+    completeBtn.addEventListener('click', () => completeTask(task.id));
+    const editBtn = taskItem.querySelector('.edit-btn');
+    editBtn.addEventListener('click', () => editTask(task.id));
+    const deleteBtn = taskItem.querySelector('.delete-btn');
+    deleteBtn.addEventListener('click', () => deleteTask(task.id));
+    taskList.appendChild(taskItem);
+
+
+function completeTask(taskId) {
+  const task = task.find(task => task.id === taskId);
+  if (task) {
+    task.completed = !task.completed;
+    renderTasks();
+  }
+}
+function editTask(taskId) {
+  const task = task.find(task => task.id === taskId);
+  if (task) {
+    const newTask = prompt('Enter a new task:', task.task);
+    if (newTask !== null) {
+      task.task = newTask.trim();
+      renderTasks();
+    }
+  }
+}
+function deleteTask(taskId) {
+  const taskIndex = tasks.findIndex(task => task.id === taskId);
+  if (taskIndex !== -1) {
+    tasks.splice(taskIndex, 1);
+    renderTasks();
+  }
+}
+addTaskForm.addEventListener('submit', addTask);
+renderTasks();
